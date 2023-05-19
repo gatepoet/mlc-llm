7e8bdf8776ba4a30222682ee16b93149a86efb4d
Yaxing Cai <caiyaxing666@gmail.com>
2023-05-19 01:30:21 -0700

    add model UI

--- ios/MLCChat/StartView.swift
+++ ios/MLCChat/StartView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct StartView: View {
     @EnvironmentObject var state: StartState
+    @State private var isAdding: Bool = false
+    @State private var inputModelUrl: String = ""
     
     var body: some View {
         NavigationStack {
@@ -17,8 +19,38 @@ struct StartView: View {
                     ForEach(state.models) { modelState in
                         ModelView().environmentObject(modelState)
                     }
+                    if !isAdding {
+                        Button("Add model") {
+                            isAdding = true
+                        }.buttonStyle(.borderless)
+                    } else {
+                        TextField("Model URL", text: $inputModelUrl)
+                    }
                 }
             }.navigationTitle("MLC Chat")
+            .toolbar{
+                if isAdding {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        Button("Cancel") {
+                            isAdding = false
+                            inputModelUrl = ""
+                        }
+                        .opacity(0.9)
+                        .padding()
+                    }
+                    if !inputModelUrl.isEmpty {
+                        ToolbarItem(placement: .navigationBarTrailing) {
+                            Button("Add") {
+                                state.addModel(modelRemoteBaseUrl: inputModelUrl)
+                                isAdding = false
+                                inputModelUrl = ""
+                            }
+                            .opacity(0.9)
+                            .padding()
+                        }
+                    }
+                }
+            }
         }
     }
 }
